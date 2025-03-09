6a4796e9388749fd31ad40fca591e4209dbaa38d
Hatem Soliman <hatemthedev@gmail.com>
2025-03-09 19:30:41 +0200

    Indented correctly

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ struct timespec get_current_time()
 double time_diff_ms(struct timespec start, struct timespec end)
 {
   return (end.tv_sec - start.tv_sec) * 1000.0 +
-         (end.tv_nsec - start.tv_nsec) / 1000000.0;
+        (end.tv_nsec - start.tv_nsec) / 1000000.0;
 }
 
 int main()
